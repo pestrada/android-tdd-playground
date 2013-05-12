e6bf59a99c82f4d0a379b6c6d29d68ca97813c45
pestrada <paul_e_mtz@hotmail.com>
2013-05-12 16:14:35 -0500

    adds sample test

--- src/org/pestrada/android_tdd_playground/MainActivity.java
+++ src/org/pestrada/android_tdd_playground/MainActivity.java
@@ -2,6 +2,8 @@ package org.pestrada.android_tdd_playground;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
 
@@ -9,6 +11,11 @@ public class MainActivity extends FragmentActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+  
+  public void changeText(View view) {
+    TextView textView = (TextView) findViewById(R.id.textView1);
+    textView.setText("new text");
   }
 
 }
