cd86d977d92bd905ab786a87b16d99360d247545
Paul Estrada <paul_e_mtz@hotmail.com>
2015-12-03 15:52:23 -0600

    new android studio structure

---
+++ app/src/main/java/org/pestrada/android_tdd_playground/MainActivity.java
@@ -0,0 +1,13 @@
+package org.pestrada.android_tdd_playground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
