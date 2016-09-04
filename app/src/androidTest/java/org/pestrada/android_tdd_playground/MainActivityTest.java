3e08d61c4f7916ab7de8beb43c9862d5735cc579
Paul Estrada <paul_e_mtz@hotmail.com>
2016-09-04 12:51:46 -0500

    unlock screen in tests

--- app/src/androidTest/java/org/pestrada/android_tdd_playground/MainActivityTest.java
+++ app/src/androidTest/java/org/pestrada/android_tdd_playground/MainActivityTest.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.WindowManager;
 import android.widget.Button;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,11 +24,23 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityTest {
-    Activity mainActivity = null;
-    Button button = null;
+    Activity activity = null;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        activity = mActivityRule.getActivity();
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
+    }
 
     @Test
     public void testChangeText() {
