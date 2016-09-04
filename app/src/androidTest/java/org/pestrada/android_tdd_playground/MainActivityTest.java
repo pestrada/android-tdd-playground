e0258d382a01a30370768ab900e9e03594e94276
Paul Estrada <paul_e_mtz@hotmail.com>
2016-09-04 00:29:35 -0500

    run tests using espresso

--- app/src/androidTest/java/org/pestrada/android_tdd_playground/MainActivityTest.java
+++ app/src/androidTest/java/org/pestrada/android_tdd_playground/MainActivityTest.java
@@ -1,36 +1,39 @@
 package org.pestrada.android_tdd_playground;
 
 import android.app.Activity;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.TouchUtils;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.widget.Button;
-import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by LAP02 on 03/12/2015.
  */
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
     Activity mainActivity = null;
     Button button = null;
 
-    public MainActivityTest(Class<MainActivity> activityClass) {
-        super(activityClass);
-    }
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
-    public MainActivityTest() {
-        super(MainActivity.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        mainActivity = getActivity();
-    }
-
+    @Test
     public void testChangeText() {
-        button = (Button) mainActivity.findViewById(R.id.button_test);
-        TouchUtils.clickView(this, button);
-        TextView view = (TextView) mainActivity.findViewById(R.id.textView1);
-        assertEquals("new text", view.getText().toString());
+        onView(withId(R.id.button_test))
+                .perform(click());
+
+        onView(withId(R.id.textView1))
+                .check(matches(withText("new text")));
     }
 }
